1ecf6256ff9583dc6331a6a8b65533d2b3eb2d15
Mateo <mateo.bertello@gmail.com>
2023-04-24 14:14:24 -0300

    Arreglando deploy 2

--- src/components/home/MovieListHome.jsx
+++ src/components/home/MovieListHome.jsx
@@ -3,7 +3,6 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import './movieHomeCard.scss';
-import { MovieHomeCard } from './movieHomeCard';
 
 export const MovieListHome = ({ moviesArray, title }) => {
     const settings = {
