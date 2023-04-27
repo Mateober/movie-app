76f840f2cd40b525a310b492cf10e3f72cf43c4a
Mateo <mateo.bertello@gmail.com>
2023-04-27 12:35:21 -0300

    Terminando loginPage

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,42 +1,73 @@
+import { useState } from 'react';
+import '../sass/loginPage.scss';
+import { FcGoogle } from 'react-icons/fc';
+import { ImFacebook2 } from 'react-icons/im';
+
 export const LoginPage = () => {
+    const [registerVisible, setRegisterVisible] = useState(false);
+
     return (
-        <div className="containerLoginPage">
-            <div className="containerLoginPage2">
-                <div className="containerLoginPage__login">
-                    <h3>Ingreso</h3>
+        <div className="LoginPage">
+            <div className="LoginPage2">
+                <div className={`container__login ${registerVisible ? 'hidden' : ''}`}>
+                    <h3>Ingresar con</h3>
+                    <div className="buttonsContainer">
+                        <div className="btnLogin buttonsContainer__google">
+                            <FcGoogle />
+                            Google
+                        </div>
+                        <div className="btnLogin buttonsContainer__facebook">
+                            <ImFacebook2 />
+                            Facebook
+                        </div>
+                    </div>
                     <form>
-                        <div>
-                            <input type="email" placeholder="Correo" name="loginEmail" />
+                        <div className="inputLoginPage">
+                            <p>Email</p>
+                            <input type="email" name="loginEmail" />
                         </div>
-                        <div>
-                            <input type="password" placeholder="Contraseña" name="loginPassword" />
+                        <div className="inputLoginPage">
+                            <p>
+                                Password <span>Olvidaste tu contraseña?</span>{' '}
+                            </p>
+                            <input type="password" name="loginPassword" />
                         </div>
-                        <div>
-                            <input type="submit" className="btnSubmit" value="Login" />
+                        <div className="inputLoginPage">
+                            <input type="submit" className="btnSubmit" value="Ingresar" />
                         </div>
+                        <p className="inputLoginPage__register" onClick={() => setRegisterVisible(!registerVisible)}>
+                            Aun no te has registrado? <span>Registrate ahora</span>{' '}
+                        </p>
                     </form>
                 </div>
 
-                <div className="containerLoginPage__register">
+                <div className={`container__login ${registerVisible ? '' : 'hidden'}`}>
                     <h3>Registro</h3>
                     <form>
-                        <div>
-                            <input type="text" placeholder="Nombre" name="registerName" />
+                        <div className="inputLoginPage">
+                            <p>Nombre</p>
+                            <input type="text" name="registerName" />
                         </div>
-                        <div>
-                            <input type="email" placeholder="Correo" name="registerEmail" />
+                        <div className="inputLoginPage">
+                            <p>Email</p>
+                            <input type="email" name="registerEmail" />
                         </div>
-                        <div>
-                            <input type="password" placeholder="Contraseña" name="registerPassword" />
+                        <div className="inputLoginPage">
+                            <p>Contraseña</p>
+                            <input type="password" name="registerPassword" />
                         </div>
 
-                        <div>
-                            <input type="password" placeholder="Repita la contraseña" name="registerPassword2" />
+                        <div className="inputLoginPage">
+                            <p>Repita la contraseña</p>
+                            <input type="password"  name="registerPassword2" />
                         </div>
 
-                        <div>
+                        <div className="inputLoginPage">
                             <input type="submit" className="btnSubmit" value="Crear cuenta" />
                         </div>
+                        <p className="inputLoginPage__register" onClick={() => setRegisterVisible(!registerVisible)}>
+                            Ya estas registrado? <span>Ingresa ahora</span>
+                        </p>
                     </form>
                 </div>
             </div>
