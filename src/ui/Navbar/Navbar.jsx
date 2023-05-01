1885f9f061e270d6fb8b8fdb5a02e225d4013194
Mateo <mateo.bertello@gmail.com>
2023-05-01 11:54:08 -0300

    Agregando linea navbar

--- src/ui/Navbar/Navbar.jsx
+++ src/ui/Navbar/Navbar.jsx
@@ -26,39 +26,22 @@ const Navbar = () => {
         <>
             <nav className="navbar">
                 <div className="container">
-                    <div className="logo">
-                        <div className="icon">
-                            <BiCameraMovie />
-                        </div>
+                    <NavLink to={`/home`} className="logo">
+                        <div className="icon"><BiCameraMovie /></div>
                         <p>MovieApp</p>
+                    </NavLink>
+
+                    <div className={`menu ${showMenu ? 'show-menu' : ''}`}>
+                        <NavLink to={`/home`}>HOME</NavLink>
+                        <NavLink to={`/show/movie`} onClick={() => { handleCategoryTypeClick('movie');}}>MOVIES</NavLink>
+                        <NavLink to={`/show/tv`} onClick={() => { handleCategoryTypeClick('tv');}}>TV SHOWS</NavLink>
                     </div>
-                    <ul className={`menu ${showMenu ? 'show-menu' : ''}`}>
-                        <NavLink to={`/home`}>Home</NavLink>
-                        <NavLink
-                            to={`/show/movie`}
-                            onClick={() => {
-                                handleCategoryTypeClick('movie');
-                            }}
-                        >
-                            Movies
-                        </NavLink>
-                        <NavLink
-                            to={`/show/tv`}
-                            onClick={() => {
-                                handleCategoryTypeClick('tv');
-                            }}
-                        >
-                            TV Shows
-                        </NavLink>
-                    </ul>
                     <InputSearch />
-                    <NavLink to={`/login`}><button className="login-button" >Login</button></NavLink>
-                    
-                    <div className="menu-icon" onClick={toggleMenu}>
-                        <FaBars />
-                    </div>
+                    <NavLink to={`/login`}><button className="login-button">Login</button></NavLink>
+                    <div className="menu-icon" onClick={toggleMenu}><FaBars /></div>
                 </div>
             </nav>
+
             <nav className="navbarMobile">
                 <div>
                     <h1>NAVBAR</h1>
