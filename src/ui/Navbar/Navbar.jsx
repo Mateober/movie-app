f469f1ec915540cb69d740a7c1668c79fca13b2a
Mateo <mateo.bertello@gmail.com>
2023-05-02 12:16:56 -0300

    Mejorando navbarmobile

--- src/ui/Navbar/Navbar.jsx
+++ src/ui/Navbar/Navbar.jsx
@@ -1,14 +1,18 @@
 import React, { useState } from 'react';
-import { FaBars } from 'react-icons/fa';
 import { BiCameraMovie } from 'react-icons/bi';
+import { MdHome } from 'react-icons/md';
 import './navbar.scss';
+import './navbarMobile.scss';
 import { InputSearch } from '../Search/InputSearch';
 import { NavLink } from 'react-router-dom';
 import { useMoviesStore } from '../../hooks/useMoviesStore';
+import { FaSearch } from 'react-icons/fa';
+import { BiMovie } from 'react-icons/bi';
+import { RiTvLine } from 'react-icons/ri';
 
 const Navbar = () => {
     const [showMenu, setShowMenu] = useState(false);
-    // Función toggleMenu para cambiar el valor de showMenu
+
     const toggleMenu = () => {
         setShowMenu(!showMenu);
     };
@@ -27,24 +31,57 @@ const Navbar = () => {
             <nav className="navbar">
                 <div className="container">
                     <NavLink to={`/home`} className="logo">
-                        <div className="icon"><BiCameraMovie /></div>
+                        <div className="icon">
+                            <BiCameraMovie />
+                        </div>
                         <p>MovieApp</p>
                     </NavLink>
-
-                    <div className={`menu ${showMenu ? 'show-menu' : ''}`}>
+                    <div className="menu">
                         <NavLink to={`/home`}>HOME</NavLink>
-                        <NavLink to={`/show/movie`} onClick={() => { handleCategoryTypeClick('movie');}}>MOVIES</NavLink>
-                        <NavLink to={`/show/tv`} onClick={() => { handleCategoryTypeClick('tv');}}>TV SHOWS</NavLink>
+                        <NavLink
+                            to={`/show/movie`}
+                            onClick={() => {
+                                handleCategoryTypeClick('movie');
+                            }}
+                        >
+                            MOVIES
+                        </NavLink>
+                        <NavLink
+                            to={`/show/tv`}
+                            onClick={() => {
+                                handleCategoryTypeClick('tv');
+                            }}
+                        >
+                            TV SHOWS
+                        </NavLink>
                     </div>
                     <InputSearch />
-                    <NavLink to={`/login`}><button className="login-button">Login</button></NavLink>
-                    <div className="menu-icon" onClick={toggleMenu}><FaBars /></div>
+                    <NavLink to={`/login`}>
+                        <button className="login-button">Login</button>
+                    </NavLink>
                 </div>
             </nav>
 
             <nav className="navbarMobile">
-                <div>
-                    <h1>NAVBAR</h1>
+                <div className="container">
+                    <NavLink to={`/home`} className="logo">
+                        <div className="icon">
+                            <BiCameraMovie />
+                        </div>
+                        <p>MovieApp</p>
+                    </NavLink>
+                    <div className="menu">
+                        <MdHome className="menu__homeIcon" />
+                        <p>Home</p>
+                        <BiMovie className="menu__movieIcon" />
+                        <p>Movies</p>
+                        <RiTvLine className="menu__tvIcon" />
+                        <p>Tv Shows</p>
+                        <FaSearch className="menu__searchIcon" />
+                        <p>Busqueda</p>
+                    </div>
+
+                    <div className="user-icon"></div>
                 </div>
             </nav>
         </>
