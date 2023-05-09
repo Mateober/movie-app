976854d9387fae241698ab6e835fb4d933c829d0
Mateo <mateo.bertello@gmail.com>
2023-05-09 14:38:42 -0300

    Empezando authSlice

---
+++ src/hooks/useAuthStore.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { clearErrorMessage, onChecking, onLogin, onLogout } from '../store/auth/authSlice';
+import { useDispatch, useSelector } from 'react-redux';
+
+export const useAuthStore = () => {
+    const dispatch = useDispatch();
+    const startLogin = async ({ email, password }) => {};
+    const startRegister = async ({ name, surname, email, password }) => {};
+    const checkAuthToken = async () => {};
+    const startLogout = async () => {};
+    return {
+        startLogin,
+        startRegister,
+        checkAuthToken,
+        startLogout,
+    };
+};
--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { moviesSlice } from './movies/moviesSlice';
+import { authSlice } from './auth/authSlice';
 
 export const store = configureStore({
     reducer: {
         movies: moviesSlice.reducer,
+        auth: authSlice.reducer,
     },
 });
